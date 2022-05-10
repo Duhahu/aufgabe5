9bd3250b96f97103786b0655111b70b32a67ca12
Duhahu <dohahoseen20@gmail.com>
2022-05-10 17:50:30 +0200

    ...

---
+++ src/test/java/de/hfu/UtilTest.java
@@ -0,0 +1,11 @@
+package de.hfu;
+import org.junit.Test;
+
+public class UtilTest {
+	
+	@Test(expected=IllegalArgumentException.class)
+    public void testUtilCase() {
+		
+		Util.istErstesHalbjahr(20);
+}
+}
